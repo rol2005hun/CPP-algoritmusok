6c3d46e761b0ee64114760ac584b705607094a64
rol2005hun <81093097+rol2005hun@users.noreply.github.com>
2023-03-09 20:04:53 +0200

    Create binariskereses.cpp

---
+++ binariskereses.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int v[101], n, x, ok; // deklarálunk: egy 100 elemű vektort; hány eleme van; melyik számot keressük; és egy oké változót
+    
+    cin >> n >> x; // beolvassuk az n számot
+    
+    for(int i = 1; i <= n; i++) {
+        cin >> v[i]; // beolvassuk a vektort
+    }
+    
+    do {
+        ok = 1;
+        for(int i = 1; i < n; i++) {
+            if(v[i] > v[i + 1]) {
+                swap(v[i], v[i + 1]); // buborék módszer
+                ok = 0;
+            }
+        }
+    } while(!ok);
+    
+    int left, right, mid, t; // deklaráljuk a bináris kereséshez szükséges left mid és right változókat; t = talált változó
+    left = 1, right = n;
+    
+    while(left <= right) { // bináris keresés
+        mid = left + (right - left) / 2;
+        if(v[mid] == x) {
+            t = 1;
+        }
+        if(v[mid] < x) {
+            left = mid + 1;
+        } else {
+            right = mid - 1;
+        }
+    }
+    
+    if(t == 1) cout << "Megtaláltam a vektorban a(z) " << x << "-s számot.";
+    if(t == 0) cout << "Nem találtam meg a vektorban a(z) " << x << "-s számot.";
+
+    return 0;
+}
