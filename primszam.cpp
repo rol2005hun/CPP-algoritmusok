e96f2cabea6e4f7663e4519022908e9260db610d
rol2005hun <81093097+rol2005hun@users.noreply.github.com>
2023-03-12 18:20:46 +0200

    Create primszam.cpp

---
+++ primszam.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n, prim = 1;
+    cin >> n; // beolvassuk az n számot
+    
+    if(n < 2) prim = 0;
+    for(int i = 2; i * i <= n; i++) { // elmegy n gyökéig, és ellenőrzi hogy prím szám-e
+        if(n % i == 0) prim = 0;
+    }
+    
+    if(prim) cout << "Prím szám.";
+    if(!prim) cout << "Nem prím szám.";
+
+    return 0;
+}
